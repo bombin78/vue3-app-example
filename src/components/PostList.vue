cc21b051b834e859637aa7095a57fddfab38a596
bombin78 <bombin78@mail.ru>
2022-09-29 20:13:43 +0300

    Анимация списка постов

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,24 +1,25 @@
 <template>
   <div v-if="posts.length > 0">
+
     <h3>Список постов</h3>
-    <post-item
-      class="post"
-      v-for="post in posts"
-      :post="post"
-      key="post.id"
-      @remove-post="$emit('removePost', post)"
-    />
+
+    <transition-group name="posts">
+      <post-item
+        class="post"
+        v-for="post in posts"
+        :post="post"
+        key="post.id"
+        @remove-post="$emit('removePost', post)"
+      />
+    </transition-group>
+
   </div>
-  <div
-    class="empty"
-    v-else
-  >
-    Список постов пуст
-  </div>
+
+  <div class="empty" v-else>Список постов пуст</div>
 </template>
 
 <script>
-import PostItem from './PostItem.vue';
+import PostItem from "./PostItem.vue";
 
 export default {
   components: {
@@ -34,11 +35,30 @@ export default {
 </script>
 
 <style scoped>
-  .empty {
-    margin-top: 30px;
 
-    color: red;
-    font-size: 24px;
-    text-align: center;
-  }
+.empty {
+  margin-top: 30px;
+
+  color: red;
+  font-size: 24px;
+  text-align: center;
+}
+
+.posts-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.posts-enter-active,
+.posts-leave-active {
+  transition: all 0.5s ease;
+}
+.posts-enter-from,
+.posts-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.posts-move {
+  transition: transform 0.5s ease;
+}
 </style>
